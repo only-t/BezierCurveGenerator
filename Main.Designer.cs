748fa453f4890908a76b18bbeb72ac6d21bb0610
-t- <onlyt.coding@gmail.com>
2024-03-07 19:02:25 +0100

    Main bezier curve generator.

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.MainTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.ActionTypeBtn = new System.Windows.Forms.ComboBox();
+            this.ResetBtn = new System.Windows.Forms.Button();
+            this.MainTableLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // MainTableLayout
@@ -39,16 +44,70 @@
             this.MainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.MainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.MainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.MainTableLayout.Controls.Add(this.MainPictureBox, 1, 1);
+            this.MainTableLayout.Controls.Add(this.ActionTypeBtn, 3, 1);
+            this.MainTableLayout.Controls.Add(this.ResetBtn, 3, 2);
             this.MainTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainTableLayout.Location = new System.Drawing.Point(0, 0);
-            this.MainTableLayout.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.MainTableLayout.Margin = new System.Windows.Forms.Padding(0);
             this.MainTableLayout.Name = "MainTableLayout";
-            this.MainTableLayout.RowCount = 3;
+            this.MainTableLayout.RowCount = 5;
             this.MainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.MainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.MainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
             this.MainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.MainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.MainTableLayout.Size = new System.Drawing.Size(800, 450);
             this.MainTableLayout.TabIndex = 0;
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPictureBox.Location = new System.Drawing.Point(20, 20);
+            this.MainPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainTableLayout.SetRowSpan(this.MainPictureBox, 3);
+            this.MainPictureBox.Size = new System.Drawing.Size(592, 410);
+            this.MainPictureBox.TabIndex = 1;
+            this.MainPictureBox.TabStop = false;
+            this.MainPictureBox.Click += new System.EventHandler(this.MainPictureBox_Click);
+            // 
+            // ActionTypeBtn
+            // 
+            this.ActionTypeBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ActionTypeBtn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ActionTypeBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.ActionTypeBtn.Font = new System.Drawing.Font("Bahnschrift", 10F);
+            this.ActionTypeBtn.FormattingEnabled = true;
+            this.ActionTypeBtn.Items.AddRange(new object[] {
+            "Add",
+            "Remove",
+            "Move"});
+            this.ActionTypeBtn.Location = new System.Drawing.Point(645, 38);
+            this.ActionTypeBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.ActionTypeBtn.MaxDropDownItems = 2;
+            this.ActionTypeBtn.Name = "ActionTypeBtn";
+            this.ActionTypeBtn.Size = new System.Drawing.Size(121, 24);
+            this.ActionTypeBtn.TabIndex = 2;
+            this.ActionTypeBtn.SelectedIndexChanged += new System.EventHandler(this.ActionTypeBtn_SelectedIndexChanged);
+            // 
+            // ResetBtn
+            // 
+            this.ResetBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ResetBtn.AutoSize = true;
+            this.ResetBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ResetBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.ResetBtn.Font = new System.Drawing.Font("Bahnschrift", 10F);
+            this.ResetBtn.Location = new System.Drawing.Point(672, 95);
+            this.ResetBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.ResetBtn.Name = "ResetBtn";
+            this.ResetBtn.Padding = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.ResetBtn.Size = new System.Drawing.Size(67, 30);
+            this.ResetBtn.TabIndex = 3;
+            this.ResetBtn.Text = "Reset";
+            this.ResetBtn.UseVisualStyleBackColor = true;
+            this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
             // 
             // Main
             // 
@@ -59,6 +118,9 @@
             this.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Name = "Main";
             this.Text = "Bézier Curve Generator";
+            this.MainTableLayout.ResumeLayout(false);
+            this.MainTableLayout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -66,6 +128,9 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel MainTableLayout;
+        private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.ComboBox ActionTypeBtn;
+        private System.Windows.Forms.Button ResetBtn;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -1,20 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BezierCurveGenerator
 {
+    public enum ACTION_TYPE
+    {
+        ADD_POINT,
+        REMOVE_POINT,
+        MOVE_POINT
+    }
+
     public partial class Main : Form
     {
+        private List<Point> points = new List<Point>();
+        private ACTION_TYPE actType;
+        private const int BASE_RESOLUTION = 100;
+        private const int MAX_POINTS = 8;
+
         public Main()
         {
             InitializeComponent();
+            this.MinimumSize = new Size(550, 400);
+            this.ActionTypeBtn.SelectedIndex = 0;
+            this.actType = ACTION_TYPE.ADD_POINT;
+        }
+
+        private void MainPictureBox_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouseEventArgs = (MouseEventArgs)e;
+            if(this.actType == ACTION_TYPE.ADD_POINT && this.points.Count < MAX_POINTS)
+            {
+                points.Add(new Point(mouseEventArgs.X, mouseEventArgs.Y));
+
+                Bitmap btm = GenerateImage();
+                this.MainPictureBox.Image = btm;
+            }
+        }
+
+        private void ActionTypeBtn_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(sender is ComboBox comboBox)
+            {
+                this.actType = (ACTION_TYPE)comboBox.SelectedIndex;
+            }
+        }
+
+        private int ComputeBinomial(int n, int k)
+        {
+            if(k > n)
+            {
+                return 0;
+            }
+
+            if(k == 0 || k == n)
+            {
+                return 1;
+            }
+
+            return ComputeBinomial(n - 1, k - 1) + ComputeBinomial(n - 1, k);
+        }
+
+        private Bitmap GenerateImage()
+        {
+            Bitmap bitmap = new Bitmap(this.MainPictureBox.Width, this.MainPictureBox.Height);
+            Color black = Color.Black;
+
+            foreach (Point p in this.points)
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        try
+                        {
+                            bitmap.SetPixel(p.X + i, p.Y + j, black);
+                        } catch (ArgumentOutOfRangeException) { }
+                    }
+                }
+            }
+
+            int order = this.points.Count - 1;
+
+            switch(order)
+            {
+                case 0:
+                    return bitmap;
+                case 1:
+
+                    return bitmap;
+                default:
+                    break;
+            }
+
+            int resolution = BASE_RESOLUTION * (order / 2);
+            Console.WriteLine(resolution);  
+            for (double i = 0; i <= resolution; i++)
+            {
+                double x = 0, y = 0;
+
+                for (int j = 0; j <= order; j++)
+                {
+                    int binomial = ComputeBinomial(order, j);
+                    double t = i / resolution;
+                    x += binomial * Math.Pow(1 - t, order - j) * Math.Pow(t, j) * this.points[j].X;
+                    y += binomial * Math.Pow(1 - t, order - j) * Math.Pow(t, j) * this.points[j].Y;
+                }
+
+                bitmap.SetPixel((int)x, (int)y, black);
+            }
+
+            return bitmap;
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            this.points.Clear();
+            this.MainPictureBox.Image = new Bitmap(this.MainPictureBox.Width, this.MainPictureBox.Height);
         }
     }
 }
